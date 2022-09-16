f8dbcd1067420b62b0c3fac76e0dff31869e5be5
konosuke.matsuura <konosuke.matsuura@optim.co.jp>
2022-09-16 16:29:20 +0900

    Add a full_title helper

--- tutorial_003/sample_app/spec/system/static_pages_spec.rb
+++ tutorial_003/sample_app/spec/system/static_pages_spec.rb
@@ -9,22 +9,22 @@ RSpec.describe "StaticPages UI Test", type: :system do
 
   it "/static_pages/home title OK?" do
     visit "/static_pages/home"
-    expect(page).to have_title "Home | #{base_title}"
+    expect(page.title).to eq "#{base_title}"
   end
 
   it "/static_pages/help title OK?" do
     visit "/static_pages/help"
-    expect(page).to have_title "Help | #{base_title}"
+    expect(page.title).to eq "Help | #{base_title}"
   end
 
   it "/static_pages/about title OK?" do
     visit "/static_pages/about"
-    expect(page).to have_title "About | #{base_title}"
+    expect(page.title).to eq "About | #{base_title}"
   end
 
   it "/static_pages/contact title OK?" do
     visit "/static_pages/contact"
-    expect(page).to have_title "Contact | #{base_title}"
+    expect(page.title).to eq "Contact | #{base_title}"
   end
 
 #  pending "add some scenarios (or delete) #{__FILE__}"
